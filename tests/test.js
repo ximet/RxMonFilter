42963200fc5c81639c54c82b4838af5e931b9f16
ximet <dq.ximet@gmail.com>
2018-01-02 13:26:41 +0300

    add test for isString helper

--- tests/test.js
+++ tests/test.js
@@ -2,9 +2,31 @@ const mocha = require('mocha'); // jshint ignore:line
 const expect = require('chai').expect;
 const { isString } = require('../src/helper.js');
 
-describe("Check string", function() {
-    it('should check value is string', function () {
-        const value = "";
-        expect(isString(value)).to.equal(true);
+describe('isString', function() {
+    describe("Check empty string", function() {
+        it('should check empty string', function () {
+            const value = "";
+            expect(isString(value)).to.equal(true);
+        });
+    });
+    describe("Check udefined value", function() {
+        it('should check undefined', function () {
+            const value = undefined;
+            expect(isString(value)).to.equal(false);
+        });
+    });
+
+    describe("Check null value", function() {
+        it('should check null', function () {
+            const value = null;
+            expect(isString(value)).to.equal(false);
+        });
+    });
+
+    describe("Check correct string", function() {
+        it('should check string', function () {
+            const value = 'Null pointer Exception';
+            expect(isString(value)).to.equal(true);
+        });
     });
 });
